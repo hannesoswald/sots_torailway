62b96e129b133c37c3bd635f84bcd782dc92c3cc
Hannes Oswald <hannesoswald@Hannes-iMac-Zurich.local>
2022-10-06 22:52:53 +0200

    first commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn sots.wsgi --log-file -
---
+++ sots/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for sots project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sots.settings')
+
+application = get_asgi_application()
